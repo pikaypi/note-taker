b27c4051b4ab226e0e33ccc035ff772adf6052d9
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-21 16:08:03 -0400

    import the database to the server

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.port || 3001;
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('./public'));
 
+// Import and require the database of tasks
+const tasks = require('./db/db.json');
 
 // Create a route that will serve up the notes.html page
 app.get('/notes', (req, res) => 
