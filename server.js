58d219176be9624421747d4644006efd4d2de419
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-21 16:04:39 -0400

    switch the order of the routes so * is last

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ const PORT = process.env.port || 3001;
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('./public'));
 
-// Create a route the will serve up the index.html page
-app.get('*', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-);
 
 // Create a route that will serve up the notes.html page
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+// Create a route the will serve up the index.html page
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 // Tell the server to listen at the assigned port
 app.listen(PORT, () => 
     console.log(`Server listening at http://localhost:${PORT}`)
