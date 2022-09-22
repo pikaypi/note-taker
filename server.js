873b69287fb2594be862cc1321d11ae5dde0f4c4
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-22 16:54:16 -0400

    add delete route for /api/notes/:id

--- server.js
+++ server.js
@@ -15,9 +15,15 @@ app.use(express.static('./public'));
 
 // Add middleware for parsing application/json
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
 
 // Import and require the database of tasks
 const notes = require('./db/db.json');
+
+// Create a route that will serve up the notes.html page
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 // Create a GET route for /api/notes that returns the database as JSON
 app.get('/api/notes', (req, res) => 
@@ -29,11 +35,46 @@ app.post('/api/notes', (req, res) => {
     // Temporary log that a request was received
     const { title, text } = req.body;
 
-    const newNote = {
-        title,
-        text,
-        id: uuid()
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        };
+
+        // Read existing notes from the database
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert the file into a JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add the new note
+                parsedNotes.push(newNote);
+
+                // Write the update notes back to the file
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), (writeErr) => 
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully updated notes!')
+                );
+            }
+        });
+
+        res.json(newNote);
+    } else {
+        res.status(500).json('Error in posting note').send();
     }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    // Temporarily log that the request was received
+    console.info(`${req.method} request has been received`);
+
+    // Extract the note id from the api request url
+    const pathArray = req.originalUrl.split('/');
+    const id = pathArray[pathArray.length - 1];
 
     // Read existing notes from the database
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -43,25 +84,26 @@ app.post('/api/notes', (req, res) => {
             // Convert the file into a JSON object
             const parsedNotes = JSON.parse(data);
 
-            // Add the new note
-            parsedNotes.push(newNote);
+            // Create a variable to hold the new notes list
+            const newNotes = [];
+
+            // Iterate through the notes and add all but the note to be deleted
+            for (let i = 0; i < parsedNotes.length; i++) {
+                if (parsedNotes[i]['id'] !== id) {
+                    newNotes.push(parsedNotes[i])
+                }
+            }
 
             // Write the update notes back to the file
-            fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), (writeErr) => 
+            fs.writeFile('./db/db.json', JSON.stringify(newNotes), (writeErr) => 
                 writeErr
                     ? console.error(writeErr)
                     : console.info('Successfully updated notes!')
             );
         }
     });
-
-    res.sendFile(path.join(__dirname, 'db/db.json'))
+    res.status(201).send();
 });
-
-// Create a route that will serve up the notes.html page
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
 
 // Create a route the will serve up the index.html page
 app.get('*', (req, res) => 
