7a96c75e696f01ae3b904f92dec4f314197b70d6
Paul Pinard <paul.k.pinard@gmail.com>
2022-09-21 23:10:10 -0400

    POST method to /api/notes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-// Import and require express and path
+// Import and require modules
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const uuid = require('./helpers/uuid')
 
 // Initialize the app variable by setting it to the value of express()
 const app = express();
@@ -11,18 +13,49 @@ const PORT = process.env.port || 3001;
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('./public'));
 
+// Add middleware for parsing application/json
+app.use(express.json());
+
 // Import and require the database of tasks
-const tasks = require('./db/db.json');
+const notes = require('./db/db.json');
 
 // Create a GET route for /api/notes that returns the database as JSON
 app.get('/api/notes', (req, res) => 
-    res.json(tasks)
+    res.json(notes)
 );
 
 // Create a POST route for /api/notes that will add new notes to the database
-app.post('/api/notes', (req, res) => 
-    res.json(`${req.method} request received to save a note`)
-);
+app.post('/api/notes', (req, res) => {
+    // Temporary log that a request was received
+    const {title, text } = req.body;
+
+    const newNote = {
+        title,
+        text,
+        id: uuid()
+    }
+    
+    console.log(newNote);
+    // Read existing notes from the database
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert the file into a JSON object
+            const parsedNotes = JSON.parse(data);
+
+            // Add the new note
+            parsedNotes.push(newNote);
+
+            // Write the update notes back to the file
+            fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), (writeErr) => 
+                writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated notes!')
+            );
+        }
+    });
+});
 
 // Create a route that will serve up the notes.html page
 app.get('/notes', (req, res) => 
