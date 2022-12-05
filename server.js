57cd7c212a429fd7dc643a7e11b8ba0ce8a90be4
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-05 13:48:57 -0500

    semicolons

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(express.static('./public'));
 let notes = require('./db/db.json')
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 // Route the get request for /api/notes
 app.get('/api/notes', (req, res) => {
