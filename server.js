3b98a11050c81a78be8533cbdb0152c067d481d9
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-05 13:56:43 -0500

    add comments

--- server.js
+++ server.js
@@ -3,21 +3,28 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+// Set up the server variables
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up the middleware
 app.use(express.json());
 app.use(express.static('./public'));
+
+// Create an object to hold the database while the application is open
 let notes = require('./db/db.json')
 
+// A GET route to render the homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
+// A GET route to render the notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// An API GET route that collects and returns the notes from the databse
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         err
@@ -27,6 +34,7 @@ app.get('/api/notes', (req, res) => {
 
 });
 
+// An API POST route that creates a new note object and adds it to the database
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
 
@@ -48,6 +56,7 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+// AN API DELETE route that finds a note by id and removes it from the database
 app.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id;
     notes = notes.filter((note) => note.id != id);
@@ -59,6 +68,7 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json(notes);
 });
 
+// Start the server
 app.listen(PORT, () => 
     console.info(`Server listening at http://localhost:${PORT}`)
 );
