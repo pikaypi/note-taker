4f46d5b2243f1549a1b0957681c0202331653e0f
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-05 13:53:09 -0500

    clean up server after fixes

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ const { randomUUID } = require('crypto');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const fs = require('fs');
-const uuid = require('./helpers/uuid')
 
-// Set the app variable to express()
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Assign a port for the server
-const PORT = process.env.port || 3001;
-
-// Add middleware for parsing application/json
 app.use(express.json());
 app.use(express.static('./public'));
 let notes = require('./db/db.json')
@@ -25,7 +18,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// Route the get request for /api/notes
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         err
@@ -50,10 +42,7 @@ app.post('/api/notes', (req, res) => {
                 ? console.error(err)
                 : console.info('Succuessfully updated notes!')
         );
-    
         res.json(newNote);
-            };
-        })
     } else {
         res.status(500).json('Error in posting note')
     }
@@ -70,7 +59,6 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json(notes);
 });
 
-// Set the server to listen
 app.listen(PORT, () => 
     console.info(`Server listening at http://localhost:${PORT}`)
 );
